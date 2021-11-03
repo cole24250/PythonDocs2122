342e69be5768f4d2cc6ae2a8b3311cb827f00735
cole24250 <cole24250@live.ytech.edu>
2021-11-03 10:43:16 -0400

    Create 01_asminent.py

---
+++ 01_asminent.py
@@ -0,0 +1,44 @@
+#Cole
+#assignment examples
+
+
+
+# you can asign "values to "variables"
+#by using equals (right side goes into left side)
+x=5
+
+#when python reads a variable, name it replaces it
+#with the variables stored value
+y=x+5
+
+# there are four diffrent data types
+# intergers- any wholenumber postive or negative 
+age = 16
+# float- any number with a decimal postive or negative
+grade = 98.6
+
+#String: a string of human ewadbale characters
+name = "Cole"
+
+#numbers in a string are not numbers they are letters
+favoriteNumber= "420"
+
+#Boolean: True or false
+#true is any valuse that is not false or empty
+isSmart = True
+
+#you can output to the console by using 'print ()'
+print (age)
+print (grade)
+print(name)
+print (favoriteNumber)
+print (isSmart)
+
+#you can concatenate simliar values together
+print("My name is" + name)
+#You can use functions to convert datatypes
+print("and my age" + str(age))
+#dont forget! if you want to convert a value permanenty
+#you must assign the converted value to be a variable
+age = str(age)
+#You can convert back and forth with int(), str(), bool()
